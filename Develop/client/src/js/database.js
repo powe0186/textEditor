54a2a8cc67fa42b45ce375cc9b75b29958556c23
Ben Powell <powe0186@gmail.com>
2022-05-17 16:37:49 -0500

    Get and put methods to cached database are completed

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,47 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  //Create a connection to the database:
+  const contactDb = await openDB('jate', 1);
+
+  //create a new transaction:
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open the object store.
+  const store = tx.objectStore('jate');
+
+  //Use the .add() to store and pass the text:
+  const request = store.add({ content: content })
+
+  //get confirmation
+  const result = await request;
+  console.log('Data added to database', result);
+}
+
+//console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  //Create a connection:
+  const contactDb = await openDB('jate', 1);
+
+  //create new transaction:
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  //open the object store.
+  const store = tx.objectStore('jate');
+
+  //Use the .getAll() to get all the data
+  const request = store.getAll();
+
+  //get confirmation of request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+
+//console.error('getDb not implemented');
 
 initdb();
